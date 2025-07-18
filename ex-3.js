13b46322654202949a670267c85d5d8f2f1bbf57
lalinyuuu <lullalin.yu@gmail.com>
2025-07-18 14:50:14 +0700

    ex 1-6

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,22 @@
 // Start coding here
+// เขัยนแบบ   arrow Arrow Function// เขียนสั้นกว่า Expressionและไม่มี function keywordมั้ง
+const add = (a, b) => {
+    return a + b;
+  };
+  
+  const subtract = (a, b) => {
+    return a - b;
+  };
+  
+  const multiply = (a, b) => {
+    return a * b;
+  };
+  
+  const divide = (a, b) => {
+    return a / b;
+  };
+  
+  console.log(add(10, 5));
+  console.log(subtract(10, 5));
+  console.log(multiply(10, 5));
+  console.log(divide(10, 5));
